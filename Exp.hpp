17b142ef853f2a917a426849ecfc7ce925930676
Urtats-Code <urtats2016@gmail.com>
2024-05-01 16:49:40 +0200

    Para el Edonon

--- Exp.hpp
+++ Exp.hpp
@@ -1,15 +1,17 @@
 #ifndef EXP_HPP_
 #define EXP_HPP_
 
+
+
 struct expresionstruct {
-  std::string str;
-  std::vector<int> trues;
-  std::vector<int> falses;
+  string str;
+  vector<int> trues;
+  vector<int> falses;
 };
 
 struct sentences{
-  std::vector<int> exits;
-  std::vector<int> continues;
+  vector<int> exits;
+  vector<int> continues;
 }; 
 
 #endif /* EXP_HPP_ */
